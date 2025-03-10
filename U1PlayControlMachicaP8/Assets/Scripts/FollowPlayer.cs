1a21d3760ce5749be1b6e56b614d978a90687979
swagdog13 <727159@student.dusd.net>
2025-03-10 15:29:56 -0700

    sdfljhsdihg
    I added oncoming vehicles

--- U1PlayControlMachicaP8/Assets/Scripts/FollowPlayer.cs
+++ U1PlayControlMachicaP8/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 6, -12);
+    public Vector3 offset = new Vector3(0, 6, -12);
     // Start is called before the first frame update
     void Start()
     {
--- U1PlayControlMachicaP8/Assets/Scripts/PlayerController.cs
+++ U1PlayControlMachicaP8/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +27,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
